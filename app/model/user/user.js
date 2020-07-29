456c2e7f540c40f51d3172dd0d5705ea816f3ea5
luxiaojun <xiaojun.lu@supplyframe.cn>
2020-07-29 14:12:25 +0800

    update model

--- app/model/user/user.js
+++ app/model/user/user.js
@@ -1,14 +1,47 @@
 'use strict';
 
-module.exports = app => {
-  const userSchema = require('../../../schema/user.js')(app.Sequelize);
-
-  const User = app.model.define('user', userSchema, {
-    tableName: 'user',
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  });
+module.exports = (app) => {
+  const { STRING, INTEGER, DATE, ENUM } = app.Sequelize;
+  
+  const User = app.model.define(
+    'user',
+    {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: 'ID',
+      },
+      username: {
+        type: STRING(256),
+        unique: true,
+        allowNull: false,
+        comment: '账号',
+      },
+      gender: {
+        type: ENUM('male', 'female', 'secret'),
+        allowNull: false,
+        defaultValue: 'secret',
+        comment: '性别',
+      },
+      created_at: {
+        type: DATE,
+        allowNull: false,
+        comment: '创建时间',
+      },
+      updated_at: {
+        type: DATE,
+        allowNull: false,
+        comment: '更新时间',
+      },
+    },
+    {
+      tableName: 'user',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
 
   return User;
 };
